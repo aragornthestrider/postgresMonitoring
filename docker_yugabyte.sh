1c889d7de429342ba2a867116c1742f6752d5f21
aragornthestrider <82564721+aragornthestrider@users.noreply.github.com>
2023-07-05 14:57:10 +0530

    Added multi node yugabyte cluster

---
+++ docker_yugabyte.sh
@@ -0,0 +1,25 @@
+docker network create -d bridge testnetwork
+
+#single node cluster
+
+docker run -d --name yugabyte --net=testnetwork -h=yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start \
+ --base_dir=/tmp/ybd \
+ --master_flags "ysql_num_shards_per_tserver=4" \
+ --tserver_flags "ysql_num_shards_per_tserver=4,follower_unavailable_considered_failed_sec=30" \
+ --daemon=false
+
+#node 1
+docker run -d --name yugabyte1 --net=testnetwork -h=yugabyte1 -p7001:7000 yugabytedb/yugabyte:latest bin/yugabyted start \
+  --base_dir=/tmp/ybd \
+  --join=yugabyte \
+  --master_flags "ysql_num_shards_per_tserver=4" \
+  --tserver_flags "ysql_num_shards_per_tserver=4,follower_unavailable_considered_failed_sec=30" \
+  --daemon=false
+
+#node 2
+docker run -d --name yugabyte2 --net=testnetwork -h=yugabyte2 -p7002:7000 yugabytedb/yugabyte:latest bin/yugabyted start \
+  --base_dir=/tmp/ybd \
+  --join=yugabyte \
+  --master_flags "ysql_num_shards_per_tserver=4" \
+  --tserver_flags "ysql_num_shards_per_tserver=4,follower_unavailable_considered_failed_sec=30" \
+  --daemon=false
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 My attempt at monitoring postgres through grafana and datadog
+
+yugabyte monitoring
+https://www.yugabyte.com/blog/monitoring-yugabytedb-with-prometheus-and-grafana-in-kubernetes/
